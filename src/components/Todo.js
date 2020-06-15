ed91bb336e0b38ef7f64e0e15cb2e4e4e664f72f
zubairSheikh97 <max.zubi@gmail.com>
2020-06-15 22:45:36 +0530

    Just starting

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function TDL(){
+    return(
+        <div className="list">
+            <input type="checkbox" name="check" id=""/><span>Buy a car</span><br/>
+            <input type="checkbox" name="check" id=""/><span>Feed cat</span><br/>
+            <input type="checkbox" name="check" id=""/><span>Buy PS4</span><br/>
+        </div>
+    )
+}
+export default TDL
